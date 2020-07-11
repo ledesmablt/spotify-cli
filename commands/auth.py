89e873a2cbe807e3236fc56e62e7d105e768e538
Benj Ledesma <benj.ledesma@gmail.com>
2020-07-11 18:08:27 +0800

    added auth status function and expires_on field in credentials

--- commands/auth.py
+++ commands/auth.py
@@ -1,5 +1,6 @@
 import os
 import json
+import time
 import urllib.parse as ul
 from uuid import uuid1
 
@@ -9,14 +10,14 @@ import requests
 HOME = os.path.expanduser('~')
 CONFIG_DIR = os.path.join(HOME, '.config')
 CREDS_DIR = os.path.join(CONFIG_DIR, 'spotify-cli')
-if not os.path.exists(CONFIG_DIR):
-    os.mkdir(CONFIG_DIR)
-if not os.path.exists(CREDS_DIR):
-    os.mkdir(CREDS_DIR)
-
 CREDS_PATH = os.path.join(CREDS_DIR, 'credentials.json')
+for folder in [CONFIG_DIR, CREDS_DIR]:
+    if not os.path.exists(folder):
+        os.mkdir(folder)
 
 # auth
+CLIENT_ID = '3e96f0ef8d6d4e0994e15bf2b168235f'
+AUTH_STATUS_URL = 'https://api.spotify.com/v1/me'
 REFRESH_URI = 'https://asia-east2-spotify-cli-283006.cloudfunctions.net/auth-refresh'
 REDIRECT_URI = 'https://asia-east2-spotify-cli-283006.cloudfunctions.net/auth-redirect'
 AUTH_SCOPES = [
@@ -29,7 +30,7 @@ AUTH_SCOPES = [
 ]
 AUTH_URL = (
     'https://accounts.spotify.com/authorize'
-    '?client_id=3e96f0ef8d6d4e0994e15bf2b168235f'
+    '?client_id=' + CLIENT_ID +
     '&response_type=code'
     '&redirect_uri=' + ul.quote_plus(REDIRECT_URI) +
     '&scope=' + ul.quote_plus(' '.join(AUTH_SCOPES)) +
@@ -37,14 +38,31 @@ AUTH_URL = (
 )
 
 
-def refresh(auth_code=None, refresh_token=None):
+def get_credentials():
+    if not os.path.exists(CREDS_PATH):
+        return {}
+
+    with open(CREDS_PATH) as f:
+        creds = json.load(f)
+
+    return creds
+
+
+def _is_expired():
+    creds = get_credentials()
+    return int(time.time()) > creds['expires_on']
+
+
+def refresh(auth_code=None):
     post_data = {}
     if auth_code:
         post_data = {'auth_code': auth_code}
-    elif refresh_token:
-        post_data = {'refresh_token': refresh_token}
     else:
-        raise Exception('Please supply an authorization code or refresh token.')
+        refresh_token = get_credentials().get('refresh_token')
+        if refresh_token:
+            post_data = {'refresh_token': refresh_token}
+        else:
+            raise Exception('Please supply an authorization code or refresh token.')
 
     refresh_res = requests.post(REFRESH_URI, json=post_data)
     try:
@@ -53,6 +71,7 @@ def refresh(auth_code=None, refresh_token=None):
     except:
         raise Exception('Error in obtaining access token. Please try again.')
 
+    refresh_data['expires_on'] = int(time.time()) + refresh_data['expires_in']
     return refresh_data
 
 
@@ -79,9 +98,25 @@ def login():
     return
 
 
+def status():
+    creds = get_credentials()
+    if _is_expired():
+        print('expired!')
+        refresh(refresh_token=creds['refresh_token'])
+
+    headers = {'Authorization': 'Bearer ' + creds['access_token']}
+    res = requests.get(AUTH_STATUS_URL, headers=headers)
+    user_data = res.json()
+    try:
+        print('Logged in as {}'.format(user_data['display_name']))
+    except:
+        print('LOGIN ERROR')
+        print(user_data)
+    return
+
+
 if __name__ == '__main__':
     # test login and refresh
     login()
-    with open(CREDS_PATH) as f:
-        creds = json.load(f)
-    refresh(refresh_token=creds['refresh_token'])
+    refresh()
+    status()
