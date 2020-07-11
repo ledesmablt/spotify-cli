b33e7e48c2002f1dbbb9d2c297460144fb472e2d
Benj Ledesma <benj.ledesma@gmail.com>
2020-07-11 16:49:57 +0800

    created refresh cloud and local functions for getting access token w/ either auth code or refresh token

---
+++ cloud-functions/auth-refresh/main.py
@@ -0,0 +1,28 @@
+import json
+import requests
+from flask import jsonify
+
+REFRESH_URL = 'https://accounts.spotify.com/api/token'
+REDIRECT_URI = 'https://asia-east2-spotify-cli-283006.cloudfunctions.net/auth-redirect'
+with open('credentials.json') as f:
+    CREDENTIALS = json.load(f)
+
+def main(request):
+    request_data = request.get_json()
+    print(request_data)
+    auth_code = request_data.get('auth_code')
+    refresh_token = request_data.get('refresh_token')
+    post_data = {
+        'client_id': CREDENTIALS['client_id'],
+        'client_secret': CREDENTIALS['client_secret'],
+        'redirect_uri': REDIRECT_URI
+    }
+    if auth_code:
+        post_data['grant_type'] = 'authorization_code'
+        post_data['code'] = auth_code
+    elif refresh_token:
+        post_data['grant_type'] = 'refresh_token'
+        post_data['refresh_token'] = refresh_token
+
+    res = requests.post(REFRESH_URL, data=post_data)
+    return jsonify(res.json())
--- commands/auth.py
+++ commands/auth.py
@@ -14,6 +14,7 @@ if not os.path.exists(CREDS_DIR):
 CREDS_PATH = os.path.join(CREDS_DIR, 'spotify-cli-credentials.json')
 
 # auth
+REFRESH_URI = 'https://asia-east2-spotify-cli-283006.cloudfunctions.net/auth-refresh'
 REDIRECT_URI = 'https://asia-east2-spotify-cli-283006.cloudfunctions.net/auth-redirect'
 AUTH_SCOPES = [
    'user-read-playback-state',
@@ -33,6 +34,25 @@ AUTH_URL = (
 )
 
 
+def refresh(auth_code=None, refresh_token=None):
+    post_data = {}
+    if auth_code:
+        post_data = {'auth_code': auth_code}
+    elif refresh_token:
+        post_data = {'refresh_token': refresh_token}
+    else:
+        raise Exception('Please supply an authorization code or refresh token.')
+
+    refresh_res = requests.post(REFRESH_URI, json=post_data)
+    try:
+        refresh_data = refresh_res.json()
+        assert 'access_token' in refresh_data.keys()
+    except:
+        raise Exception('Error in obtaining access token. Please try again.')
+
+    return refresh_data
+
+
 def login():
     try:
         import webbrowser
@@ -41,17 +61,24 @@ def login():
         print('Go to the following link in your browser:\n\n\t' + AUTH_URL)
 
     auth_code = input('Enter verification code: ')
+    print('\nObtaining access token...')
+    refresh_json = refresh(auth_code)
     with open(CREDS_PATH, 'w+') as f:
         try:
             creds = json.load(f)
         except:
             creds = {}
         creds['auth_code'] = auth_code
+        creds.update(refresh_json)
         json.dump(creds, f)
 
-    print('\n\nCredentials saved to ' + CREDS_PATH)
+    print('Credentials saved to ' + CREDS_PATH)
     return
 
 
 if __name__ == '__main__':
+    # test login and refresh
     login()
+    with open(CREDS_PATH) as f:
+        creds = json.load(f)
+    refresh(refresh_token=creds['refresh_token'])
