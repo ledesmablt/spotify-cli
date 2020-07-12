bd289588b00851e6e24fd48c42d87dbc65957151
Benj Ledesma <benj.ledesma@gmail.com>
2020-07-12 13:32:08 +0800

    updated custom exceptions to use ClickException

--- utils/exceptions.py
+++ utils/exceptions.py
@@ -1,4 +1,11 @@
-class AuthorizationError(Exception):
+import click
+
+class AuthorizationError(click.ClickException):
     def __init__(self):
-        self.message = 'Please authenticate the CLI (spotify auth login)'
+        self.message = 'CLI not authenticated. (spotify auth login)'
+        super().__init__(self.message)
+
+class NoPlaybackError(click.ClickException):
+    def __init__(self):
+        self.message = 'No playback session detected. Please start Spotify on one of your devices.'
         super().__init__(self.message)
