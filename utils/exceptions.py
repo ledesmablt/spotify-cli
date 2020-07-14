267bc43dbcf8abcd77741fc76d234e801c232919
Benj Ledesma <benj.ledesma@gmail.com>
2020-07-15 00:49:50 +0800

    changed some custom error messages

--- utils/exceptions.py
+++ utils/exceptions.py
@@ -1,10 +1,10 @@
 import click
 
 
-#auth
+# auth
 class AuthorizationError(click.ClickException):
     def __init__(self):
-        self.message = 'CLI not authenticated. (spotify auth login)'
+        self.message = 'CLI not authenticated.\nPlease run the command - spotify auth login'
         super().__init__(self.message)
 
 class TokenExpired(click.ClickException):
@@ -16,7 +16,7 @@ class TokenExpired(click.ClickException):
 # playback
 class NoPlaybackError(click.ClickException):
     def __init__(self):
-        self.message = 'No playback session detected. Please start Spotify on one of your devices.'
+        self.message = 'No playback session detected.\nPlease start Spotify on one of your devices.'
         super().__init__(self.message)
 
 class PodcastNotSupported(click.ClickException):
