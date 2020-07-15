dfb3d11c4ec6ad4875dd8ef28aac598fc6121627
Benj Ledesma <benj.ledesma@gmail.com>
2020-07-15 21:56:23 +0800

    changed device group to command (default=list) w/ --switch-to flag

--- commands/devices.py
+++ commands/devices.py
@@ -4,10 +4,11 @@ from utils import Spotify
 from utils.exceptions import *
 
 
-@click.command(name='list')
+@click.command()
 @click.option('-v', '--verbose', count=True)
 @click.option('--raw', is_flag=True)
-def _list(verbose=0, raw=False):
+@click.option('-s', '--switch-to', type=str)
+def devices(verbose=0, raw=False, switch_to=''):
     res = Spotify.request('me/player/devices', method='GET')
     if raw:
         if verbose >= 0:
@@ -25,45 +26,40 @@ def _list(verbose=0, raw=False):
     )
 
     # output
-    if not verbose:
-        output_list = map(
-            lambda x: f"* {x['name']}" if x['is_active'] else f"  {x['name']}",
-            sorted(devices_list, key=lambda x: x['name'])
-        )
-        click.echo('\n'.join(output_list))
+    if not switch_to:
+        if not verbose:
+            output_list = map(
+                lambda x: f"* {x['name']}" if x['is_active'] else f"  {x['name']}",
+                sorted(devices_list, key=lambda x: x['name'])
+            )
+            click.echo('\n'.join(output_list))
 
-    if verbose == 1:
-        output_list = map(
-            lambda x: f"* {x['name']} - {x['type']}" if x['is_active'] else f"  {x['name']} - {x['type']}",
-            sorted(devices_list, key=lambda x: x['name'])
-        )
-        click.echo('\n'.join(output_list))
+        if verbose == 1:
+            output_list = map(
+                lambda x: f"* {x['name']} - {x['type']}" if x['is_active'] else f"  {x['name']} - {x['type']}",
+                sorted(devices_list, key=lambda x: x['name'])
+            )
+            click.echo('\n'.join(output_list))
+
+        return
 
 
-    return
-
-
-@click.command()
-@click.option('-n', '--name', type=str)
-@click.option('--play', is_flag=True)
-def switch(name='', play=False):
-    if not name:
-        name = ''
+    if not switch_to:
+        switch_to = ''
         # interactive prompt
         pass
 
-    devices_list = _list.callback(verbose=-1, raw=True)['devices']
-    matched_devices = [x for x in devices_list if name.lower() in x['name'].lower()]
+    matched_devices = [x for x in devices_list if switch_to.lower() in (x['name'] + x['type']).lower()]
     num_matched = len(matched_devices)
     if num_matched != 1:
         if num_matched == 0:
             click.echo(
-                f'"{name}" not found. Please select a valid device.',
+                f'"{switch_to}" not found. Please select a valid device.',
                 err=True
             )
         else:
             click.echo(
-                f'{num_matched} devices matched "{name}". Please select the device to activate below.'
+                f'{num_matched} devices matched "{switch_to}". Please select the device to activate below.'
             )
 
         # interactive prompt
@@ -73,18 +69,8 @@ def switch(name='', play=False):
     to_activate = matched_devices[0]
     post_data = {
         'device_ids': [to_activate['id']],
-        'play': play
     }
     Spotify.request('me/player', method='PUT', data=post_data)
-    print(f"Switched to {to_activate['name']}")
+    print(f"Switched to {to_activate['name']} - {to_activate['type']}")
 
     return
-        
-
-# CLI group
-@click.group()
-def devices():
-    pass
-
-devices.add_command(_list)
-devices.add_command(switch)
