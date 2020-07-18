054a86db2b3a0a5fd359278995872af73bc65d07
Benj Ledesma <benj.ledesma@gmail.com>
2020-07-18 21:09:01 +0800

    added AliasGroup for command shortcuts (i.e. spotify vol = spotify volume); renamed 'prev' to 'previous'

--- cli/commands/previous.py
+++ cli/commands/previous.py
@@ -6,7 +6,7 @@ from cli.utils import Spotify
 @click.command()
 @click.option('-v', '--verbose', count=True)
 @click.option('-q', '--quiet', is_flag=True)
-def prev(verbose=0, quiet=False):
+def previous(verbose=0, quiet=False):
     Spotify.request('me/player/previous', method='POST')
     if not quiet:
         from cli.commands.status import status
--- cli/main.py
+++ cli/main.py
@@ -1,24 +1,28 @@
 import click
 
+from cli.utils.classes import AliasedGroup
+
 from cli.commands.auth import auth
 from cli.commands.status import status
 from cli.commands.play import play
 from cli.commands.pause import pause
 from cli.commands.next import _next
-from cli.commands.prev import prev
+from cli.commands.previous import previous
 from cli.commands.devices import devices
 from cli.commands.volume import volume
 
 
-@click.group()
+# CLI group
+@click.command(cls=AliasedGroup)
 def cli():
     pass
 
+# commands
 cli.add_command(auth)
 cli.add_command(status)
 cli.add_command(play)
 cli.add_command(pause)
 cli.add_command(_next)
-cli.add_command(prev)
+cli.add_command(previous)
 cli.add_command(devices)
 cli.add_command(volume)
