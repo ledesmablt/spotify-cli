487540c2dbc0d522dfe4d7cf04a2a685c2ab5509
Benj Ledesma <benj.ledesma@gmail.com>
2020-08-01 22:41:02 +0800

    added 'Last played' to history command

--- cli/commands/history.py
+++ cli/commands/history.py
@@ -22,6 +22,7 @@ from cli.utils.exceptions import *
 def history(verbose=0, raw=False, limit=10, _return_parsed=False):
     """List your recently played tracks."""
     from tabulate import tabulate
+    from datetime import datetime
     pager = Spotify.Pager('me/player/recently-played', limit=limit)
     if raw:
         if verbose >= 0:
@@ -30,14 +31,38 @@ def history(verbose=0, raw=False, limit=10, _return_parsed=False):
         
         return pager.content
 
-    headers = ['Artist', 'Track']
+    headers = ['Last played', 'Artist', 'Track']
+    current_time = datetime.utcnow()
+
     table = []
-    for item in pager.content['items']:
+    timediffs = set()
+    for i, item in enumerate(pager.content['items']):
         parsed_item = parse_track_item_full(item['track'])
-        table.append([
+        row = [
             cut_string(', '.join(parsed_item['artists']['names']), 30),
             cut_string(parsed_item['track']['name'], 50),
-        ])
+        ]
+
+        # last played parsing
+        played_at = datetime.strptime(item['played_at'].split('.')[0], '%Y-%m-%dT%H:%M:%S')
+        timediff = current_time - played_at
+        timediff_hours = int(timediff.seconds / 3600)
+        if timediff.days >= 1:
+            timediff_str = 'Yesterday'
+        elif timediff_hours >= 1:
+            timediff_str = '{} {} ago'.format(
+                timediff_hours, 'hours' if timediff_hours > 1 else 'hour'
+            )
+        else:
+            timediff_str = 'A few minutes ago'
+
+        if timediff_str in timediffs:
+            timediff_str = ''
+        else:
+            timediffs.add(timediff_str)
+
+        row.insert(0, timediff_str)
+        table.append(row)
 
     if len(table) == 0:
         click.echo('No data available for recently played songs.', err=True)
