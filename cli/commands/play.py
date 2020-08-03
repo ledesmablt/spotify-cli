8fabac5136f588e3112e053054b0dd0bfd99af1a
Benj Ledesma <benj.ledesma@gmail.com>
2020-08-04 00:29:15 +0800

    added '--track' option in play command for search query

--- cli/commands/play.py
+++ cli/commands/play.py
@@ -2,10 +2,15 @@ import click
 
 from cli.utils import Spotify
 from cli.utils.functions import retry
-from cli.utils.exceptions import NoPlaybackError
+from cli.utils.exceptions import *
 
 
 @click.command(options_metavar='[<options>]')
+@click.option(
+    '--track', type=str,
+    help='Search for a track to play.',
+    metavar='<keyword>'
+)
 @click.option(
     '-v', '--verbose', count=True,
     help='Output more info (repeatable flag).'
@@ -24,9 +29,42 @@ from cli.utils.exceptions import NoPlaybackError
     type=click.Choice(['all', 'track', 'off'], case_sensitive=False),
     help='Turn repeat on (all/track) or off.'
 )
-def play(verbose=0, quiet=False, shuffle=None, repeat=None, _request_kwargs={}):
+def play(verbose=0, quiet=False, shuffle=None, repeat=None, track=None, _request_kwargs={}):
     """Resume playback."""
+    is_search = False
+    keywords = ''
     requests = []
+    if track:
+        is_search = True
+        search_type = 'track'
+        keywords = track
+
+    if is_search:
+        import urllib.parse as ul
+        pager = Spotify.Pager(
+            'search',
+            params={
+                'q': ul.quote_plus(keywords),
+                'type': search_type,
+            },
+            content_callback=lambda c: c[search_type+'s'],
+        )
+        if len(pager.content) == 0:
+            click.echo('No results found for "{}"'.format(keywords), err=True)
+            return
+        
+        item = pager.content['items'][0]
+        if search_type == 'track':
+            _request_kwargs = {'data': {
+                'context_uri': item['album']['uri'],
+                'offset': {
+                    'uri': item['uri'],
+                },
+            }}
+        else:
+            raise FeatureInDevelopment
+
+
     if shuffle:
         from cli.commands.shuffle import shuffle as shuffle_cmd
         requests.append(
