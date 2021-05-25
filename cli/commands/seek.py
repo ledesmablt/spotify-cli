6ca93d595cf8c2d5b867ce07551d4380a22bb73b
Yeah Nope <yeahnope@posteo.de>
2021-05-25 13:25:37 +0100

    Add relative seeking

--- cli/commands/seek.py
+++ cli/commands/seek.py
@@ -6,18 +6,34 @@ from ..utils.exceptions import *
 
 @click.command(options_metavar='[<options>]')
 @click.argument(
-    'position', type=int,
+    'mode', type=click.Choice(['to', 'forward', 'rewind'], case_sensitive=False),
+    metavar='<mode>'
+)
+@click.argument(
+    'position', type=float,
     metavar='<position>'
 )
-def seek(position):
-    """Seek to time (in ms) in current track.
+def seek(mode, position):
+    """Seek to time (in seconds) in current track.
 
-    Example: spotify seek 10000
+    Example: spotify seek to 70
     """
+    if mode == 'to':
+        new_position_ms = int(position * 1000)
+    else:
+        from cli.commands.status import status
+        status_data = status.callback(raw=True, verbose=-1)
+        progress_ms = status_data.get('progress_ms')
+        duration_ms = status_data.get('item').get('duration_ms')
+        if mode == 'forward':
+            new_position_ms = min(int(progress_ms + position * 1000), duration_ms)
+        elif mode == 'rewind':
+            new_position_ms = max(int(progress_ms - position * 1000), 0)
+
     Spotify.request(
-        'me/player/seek?position_ms={}'.format(position),
+        'me/player/seek?position_ms={}'.format(new_position_ms),
         method='PUT',
         handle_errs={404: NoPlaybackError}
     )
-    click.echo('Position set to {} ms.'.format(position))
+    click.echo('Position set to {} ms.'.format(new_position_ms))
     return
